e9437675079681bf1a68497d173d0725cc0c88b8
BonsDevelopment <ninjajesus@ymail.com>
2017-09-19 20:37:18 -0400

    Challenge for DE

---
+++ DisplayStuff.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using cMenu;
+
+namespace RockPaperScissorsChallenge
+{
+    public class DisplayStuff
+    {
+        private static hMenuItemConfig hMenu1 = new hMenuItemConfig(ConsoleColor.Green,false,'|',ConsoleColor.White);
+        private static bool loopFlag = true;
+        public DisplayStuff()
+        {
+            hMenu1.AddList(new[] {"Rock","Paper","Scissors" });
+            
+        }
+
+        public int InitializeMenu(bool SetFlag)
+        {
+            loopFlag = SetFlag;
+            int returnString = 0;
+
+            while (loopFlag)
+            {
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        hMenu1.DecrementList();
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        hMenu1.IncrementList();
+                        break;
+
+                    case ConsoleKey.Enter:
+                        returnString = hMenu1.IndexOf(hMenu1[hMenu1.selectedItem]);
+                        loopFlag = false;
+                        break;
+                      
+                }
+            }
+
+            return returnString;
+        }
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorsChallenge
+{
+    class Program
+    {
+        enum Selection
+        {
+            Rock,
+            Paper,
+            Scissors
+        }
+
+        private static DisplayStuff displayObject = new DisplayStuff();
+        private static Random rNumber = new Random();
+        private const char bufferChar = '\n';
+
+        static void Main(string[] args)
+        {
+            int playerSelection = displayObject.InitializeMenu(true);
+            Result(playerSelection);
+            Console.ReadLine();
+
+        }
+
+        private static int AiChoice()
+        {
+            return rNumber.Next(0, 2);
+        }
+
+        private static void Result(int playSel)
+        {
+            int aiTurn = AiChoice();
+           
+            Console.WriteLine(bufferChar);
+
+            Console.WriteLine("AI: " + Enum.GetName(typeof(Selection),aiTurn));
+            if ((Selection)aiTurn == (Selection)playSel)
+            {
+                Console.WriteLine("Tie!");
+            }
+
+            else if(aiTurn == (int)Selection.Rock)
+            {
+                 if((Selection)playSel == Selection.Paper)
+                    Console.WriteLine("Paper beats rock! You won!");
+                 if ((Selection)playSel == Selection.Scissors)
+                    Console.WriteLine("Rock beats scissors! You lost!");
+            }
+
+            else if (aiTurn == (int)Selection.Paper)
+            {
+                if ((Selection)playSel == Selection.Rock)
+                    Console.WriteLine("Paper beats rock! You lost!");
+                if ((Selection)playSel == Selection.Scissors)
+                    Console.WriteLine("Scissors beat paper! You won!");                   
+            }
+
+            else if (aiTurn == (int)Selection.Scissors)
+            {
+                if ((Selection)playSel == Selection.Rock)
+                    Console.WriteLine("Rock beats scissors! You lost!");
+                if ((Selection)playSel == Selection.Paper)
+                    Console.WriteLine("Scissors beat paper! You lost!");
+            }
+
+            Main(new[] { "" });
+        }
+
+       
+    }
+}
